100cb65587f118173c302f3806da20f8e27479b8
ashutosh yadav <ashutoshyadav81787@gmail.com>
2025-03-04 05:22:02 +0000

    added medus new env

--- medusa-config.ts
+++ medusa-config.ts
@@ -4,14 +4,14 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    databaseUrl: process.env.MEDUSA_DATABASE_URL!,
+  http: {
+    storeCors: process.env.STORE_CORS!,
+    adminCors: process.env.ADMIN_CORS!,
+    authCors: process.env.AUTH_CORS!,
+    jwtSecret: process.env.MEDUSA_JWT_SECRET || "supersecret",
+    cookieSecret: process.env.MEDUSA_COOKIE_SECRET || "supersecret",
+  },
   },
   modules: [
     {
