b2cfa6f134e987872e070db1448649c793abef00
ashutosh yadav <ashutoshyadav81787@gmail.com>
2025-03-07 15:47:13 +0000

    ndoenvissue

--- src/admin/lib/sdk.ts
+++ src/admin/lib/sdk.ts
@@ -1,15 +1,17 @@
 import Medusa from "@medusajs/js-sdk";
-// import { loadEnv } from "@medusajs/framework/utils";
-
-// loadEnv(process.env.BASE_URL|| 'development', process.cwd())
-
-// const baseurl:string = process.env.BASE_URL as string
-
 
 export const sdk = new Medusa({
-  baseUrl: process.env.NODE_ENV === "development"? "http://localhost:9000":"https://admin.sultanchandandsons.com",
+  baseUrl: process.env.NODE_ENV === "development"
+    ? "https://admin.sultanchandandsons.com"
+    : "https://admin.sultanchandandsons.com",
   debug: process.env.NODE_ENV === "development",
   auth: {
     type: "session",
   },
-})
+});
+
+console.log("Current NODE_ENV:", process.env.NODE_ENV,);
+console.log("Using baseUrl:", process.env.NODE_ENV === "development"
+  ? "http://localhost:9000"
+  : "https://admin.sultanchandandsons.com"
+);
