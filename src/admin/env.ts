0abcd26c95b8ad3e268fbec74b4035327cd3507d
ashutosh yadav <ashutoshyadav81787@gmail.com>
2025-06-22 13:28:26 +0000

    url chaned

--- src/admin/env.ts
+++ src/admin/env.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URl="http://localhost:9000"
-// export const    BACKEND_URl="https://admin.sultanchandandsons.com";
+// export const BACKEND_URl="http://localhost:9000"
+export const    BACKEND_URl="https://admin.sultanchandandsons.com";
 
